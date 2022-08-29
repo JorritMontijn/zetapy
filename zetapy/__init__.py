8c8202c18af6995e044fb93c60873b18c96e5a2f
Guido <guido.meijer@research.fchampalimaud.org>
2022-08-29 17:17:14 +0100

    Added plotting functionality

--- zetapy/__init__.py
+++ zetapy/__init__.py
@@ -1,13 +1,5 @@
+#from zetapy import example
+#from zetapy.example import runExampleZETA
 from zetapy.main import getZeta, getIFR
 from zetapy.msd import getMultiScaleDeriv
-from zetapy.dependencies import getPeak, getGumbel, getOnset, getTempOffset, flatten
-
-"""
-Main function to calculate ZETA: zetapy.getZeta
-
-Read me and how-to-use example: https://github.com/JorritMontijn/zetapy/
-"""
-
-__version__ = '2.6.2'
-__author__ = 'Jorrit Montijn, Guido Meijer & Alexander Heimel'
-
+from zetapy.dependencies import getPeak, getGumbel, getOnset, getTempOffset, flatten, calculatePeths
