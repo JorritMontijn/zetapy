d946e14079585a328c8ea8a05ffa5c99c5d1543c
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2023-07-18 16:13:19 +0200

    Version 2.7.2

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='zetapy',
-    version='2.7',
+    version='2.7.2',
     description='Calculates neuronal responsiveness index ZETA.',
     url='https://github.com/JorritMontijn/zetapy',
     author='Jorrit Montijn, Guido Meijer & Alexander Heimel',
-    author_email='guido.meijer@research.fchampalimaud.org',
+    author_email='guido.meijer@donders.ru.nl',
     license='GNU General Public License',
     packages=['zetapy'],
-    install_requires=[],
+    install_requires=['numpy', 'matplotlib', 'scipy'],
 
     classifiers=[
         'Programming Language :: Python :: 2',
@@ -18,5 +18,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 )
