518bf079b09b3366b3fc314504f5b712478e1deb
Guido Meijer <guido.meijer@research.fchampalimaud.org>
2023-12-01 11:59:39 +0100

    20231201 - v3.0.5 - np.round returns float bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='zetapy',
-    version='3.0.4',
+    version='3.0.5',
     description='Implementations of the ZETA family of statistical tests.',
     url='https://github.com/JorritMontijn/zetapy',
     author='Jorrit Montijn, Guido Meijer & Alexander Heimel',
